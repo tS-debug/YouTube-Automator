83b2855c3e34dfd68d1181561c732d662a925f6a
Tahira <tahira.shaukat95@gmail.com>
2022-08-11 03:30:29 +0500

    initial commit

---
+++ utube_downloader.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+from sys import argv
+
+#passing video link as the parameter
+link = argv[1]
+video = YouTube(link)
+
+#just for the thrills
+print("Title: ", video.title)
+print("Views: ", video.views)
+
+#for those of us with third world country internet speeds
+potato_internet = video.streams.filter(res='480p')
+
+#file path to save your videos
+potato_internet.download('/Users/tahirashaukat/Downloads')
+
+
+
+
+
+
